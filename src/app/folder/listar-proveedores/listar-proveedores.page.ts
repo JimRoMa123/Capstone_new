7b510eda7af88aa0881a62ac62a1395659d4b9a4
JimRoMa123 <thejokerbones@gmail.com>
2024-11-03 19:55:56 -0300

    mejoras

--- src/app/folder/listar-proveedores/listar-proveedores.page.ts
+++ src/app/folder/listar-proveedores/listar-proveedores.page.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Proovedores } from 'src/app/models/proovedores';
-import { ProovedoresService } from 'src/app/services/proovedores.service';
 import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
-import { Platform } from '@ionic/angular';
 import { OrdenDeCompra } from 'src/app/models/orden-de-compra';
-import { OrdendeCompraService } from 'src/app/services/orden-de-compra.service';
-
+import { ProveedorService } from '../../services/proveedor.service';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-listar-proveedores',
   templateUrl: './listar-proveedores.page.html',
@@ -13,83 +10,33 @@ import { OrdendeCompraService } from 'src/app/services/orden-de-compra.service';
   providers: [EmailComposer]
 })
 export class ListarProveedoresPage implements OnInit {
+  proveedores: any[] = [];
   mensajeCorreo: string = '';  
-  proovedores: Proovedores[] = [];
-  proovedorSeleccionado: Proovedores | null = null;
-  fechaEntrega: string = ''; // Cambiar a string
-  cantidad: string = '0';  
-  editarProovedor: Proovedores = new Proovedores(0, '', '', '', '', '', ''); 
-
   isModalOpen = false;
   isModalEditOpen = false;
-
-  constructor(
-    private proovedoresService: ProovedoresService,
-    private emailComposer: EmailComposer,
-    private OrdendeCompraService: OrdendeCompraService,
-    private platform: Platform,
-  ) {
-    const today = new Date();
-    
-    this.fechaEntrega = today.toISOString().substring(0, 10);
+  isLoading: boolean = true;
+  ngOnInit() {
+    this.cargarProveedores();
   }
 
-  ionViewWillEnter() {
-    this.proovedores = this.proovedoresService.obtenerProovedores();
-  }
+  constructor(private proveedorService: ProveedorService, private platform: Platform, private emailComposer: EmailComposer,) {}
 
-  setOpenModalOrden(isOpen: boolean) {
-    this.isModalOpen = isOpen;
-  }
-
-  setOpenModalEdit(isOpen: boolean) {
-    this.isModalEditOpen = isOpen;
-    console.log('Modal Edit Open State:', this.isModalEditOpen);
-    
-    if (!isOpen) {
-      this.editarProovedor = new Proovedores(0, '', '', '', '', '', '');
-    }
+  cargarProveedores() {
+    this.isLoading = true;
+    this.proveedorService.getProveedores().subscribe(
+      response => {
+        this.proveedores = response.data;
+        this.isLoading = false;  // Finaliza la carga
+      },
+      error => {
+        console.error('Error al obtener clientes:', error);
+        this.isLoading = false;  // Finaliza la carga incluso si hay un error
+      }
+    );
   }
   
-  onGuardarCambios() {
-    this.guardarCambios();
-    this.setOpenModalEdit(false); // Cerrar el modal después de guardar
-  }
-  
-
-  seleccionarProovedor(proveedor: Proovedores) {
-    this.proovedorSeleccionado = proveedor;
-    this.editarProovedor = { ...proveedor };
-  }
-
-  guardarCambios() {
-    if (this.proovedorSeleccionado) {
-      console.log('Proveedor seleccionado para guardar:', this.proovedorSeleccionado);
-      
-      const proveedorActualizado = new Proovedores(
-        this.proovedorSeleccionado.id,
-        this.editarProovedor.nombre || '',
-        this.editarProovedor.direccion || '',
-        this.editarProovedor.telefono || '',
-        this.editarProovedor.email || '',
-        this.editarProovedor.alias || '',
-        this.editarProovedor.pago || ''
-      );
-  
-      this.proovedoresService.editarProovedor(this.proovedorSeleccionado.id, proveedorActualizado);
-      console.log('Proveedor actualizado:', proveedorActualizado);
-    } else {
-      console.error('No se ha seleccionado un proveedor para editar.');
-    }
-  }
-  
-
-  
-  
-
-
-  enviarCorreo(proovedor: Proovedores | null) {
-    if (!proovedor) {
+ /* enviarCorreo(proveedores: ProveedorServices | null) {
+    if (!proveedores) {
       console.log('No hay proveedor seleccionado');
       return;
     }
@@ -113,6 +60,26 @@ export class ListarProveedoresPage implements OnInit {
   }
   
 
-  ngOnInit() {}
+*/
 
+
+enviarCorreo(proveedor: any) {
+  if (!proveedor) {
+    console.log('No hay proveedor seleccionado');
+    return;
+  }
+  console.log(proveedor);
+  if (this.platform.is('cordova') || this.platform.is('capacitor')) {
+    let email = {
+      to: proveedor.email,
+      subject: 'Orden de Compra',
+      body: `Buenos días, ${proveedor.nombre}. Nuestro cliente requiere los siguientes productos: ${this.mensajeCorreo}`,
+      isHtml: true
+    };
+    this.emailComposer.open(email);
+    this.mensajeCorreo = '';
+  } else {
+    alert('Funcionalidad de correo no disponible en el navegador.');
+  }
+}
 }
--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClientesService {
+export class ProveedorService {
+
   private apiUrl = 'http://localhost:3000'; 
 
   constructor(private http: HttpClient) {}
 
-  getClientes(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/table/cliente`);
+  getProveedores(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/table/proveedor_proveedor`);
   }
 }
